ed58b4b9f3ad7c73bcdc08ab7e55e719c7b40c5b
petboa <112291489+petboa@users.noreply.github.com>
2022-12-15 22:39:33 +0000

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
 * main - prints the numbers from 1 to 100, followed by a new line
 * but for multiples of three prints Fizz instead of the number
@@ -9,6 +10,7 @@
 int main(void)
 {
 int i;
+
 for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0 && i % 5 != 0)
@@ -17,7 +19,7 @@ printf(" Fizz");
 }
 else if (i % 5 == 0 && i % 3 != 0)
 {
-printf(" Buzz"); 
+printf(" Buzz");
 }
 else if (i % 3 == 0 && i % 5 == 0)
 {
@@ -33,5 +35,6 @@ printf(" %d", i);
 }
 }
 printf("\n");
+
 return (0);
 }
