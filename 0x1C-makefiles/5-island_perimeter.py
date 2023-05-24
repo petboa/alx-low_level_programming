b1a9bd1703493374faa42c0046dc68aa375f720d
petboa <petboa123@gmail.com>
2023-05-24 07:28:07 -0700

    c makefiles

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    """
+    Calculates the perimeter of the island described in the grid.
+
+    Args:
+        grid (list[list[int]]): A rectangular grid representing the island.
+
+    Returns:
+        int: The perimeter of the island.
+
+    Examples:
+        >>> island_perimeter([[0, 1, 0, 0],
+        ...                   [1, 1, 1, 0],
+        ...                   [0, 1, 0, 0],
+        ...                   [1, 1, 0, 0]])
+        16
+    """
+    rows = len(grid)
+    if rows == 0:
+        return 0
+
+    cols = len(grid[0])
+    perimeter = 0
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4  # Start with the assumption that all sides are exposed
+
+                # Check the neighboring cells
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 1  # Subtract one side as it is shared
+                if i < rows - 1 and grid[i + 1][j] == 1:
+                    perimeter -= 1
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 1
+                if j < cols - 1 and grid[i][j + 1] == 1:
+                    perimeter -= 1
+
+    return perimeter
